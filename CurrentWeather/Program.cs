e1ada8961e39c205ed282a4c83ec4ae7425213a0
Benzas <Benzas@DESKTOP-PKQU6UA>
2024-12-24 16:33:56 +0200

    Added option for inputting city name.

--- CurrentWeather/Program.cs
+++ CurrentWeather/Program.cs
@@ -12,21 +12,37 @@ namespace WeatherTest
         static async Task Main(string[] args)
         {
             const string APIKey = "";
-            Console.WriteLine("Enter latitude: ");
-            double lat = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter longitude: ");
-            double lon = Convert.ToDouble(Console.ReadLine());
-            await HTTPSCall(lat, lon, APIKey);
+            Console.WriteLine("1 for Latitude and Longitude, 2 for City Name");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Enter latitude: ");
+                    double lat = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Enter longitude: ");
+                    double lon = Convert.ToDouble(Console.ReadLine());
+                    await HTTPSCallLatLong(lat, lon, APIKey);
+                    break;
+                case 2:
+                    Console.WriteLine("Enter city name: ");
+                    string cityname = Console.ReadLine();
+                    await HTTPSCallName(APIKey,cityname);
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
+
         }
 
-        static async Task HTTPSCall(double lat, double lon, string apikey)
+        static async Task HTTPSCallLatLong(double lat, double lon, string apikey)
         {
             HttpClient client = new HttpClient();
             try
             {
-                string url = string.Format($"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={apikey}&units=metric");
+                string urllatlong = string.Format($"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={apikey}&units=metric");
 
-                HttpResponseMessage response = await client.GetAsync(url);
+                HttpResponseMessage response = await client.GetAsync(urllatlong);
                 response.EnsureSuccessStatusCode();
 
                 string responses = await response.Content.ReadAsStringAsync();
@@ -34,6 +50,30 @@ namespace WeatherTest
                 // Deserialize the JSON response into a WeatherApiResponse object
                 var weatherresponse = JsonSerializer.Deserialize<response>(responses);
                   
+                Console.WriteLine($"Temp: {weatherresponse.main.temp}");
+                Console.WriteLine($"Weather: {weatherresponse.weather[0].main}");
+                Console.WriteLine($"Wind: {weatherresponse.wind.gust}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        static async Task HTTPSCallName(string apikey, string cityname)
+        {
+            HttpClient client = new HttpClient();
+            try
+            {
+                string urlcity = string.Format($"https://api.openweathermap.org/data/2.5/weather?q={cityname}&appid={apikey}&units=metric");
+
+                HttpResponseMessage response = await client.GetAsync(urlcity);
+                response.EnsureSuccessStatusCode();
+
+                string responses = await response.Content.ReadAsStringAsync();
+
+                // Deserialize the JSON response into a WeatherApiResponse object
+                var weatherresponse = JsonSerializer.Deserialize<response>(responses);
+
                 Console.WriteLine($"Temp: {weatherresponse.main.temp}");
                 Console.WriteLine($"Weather: {weatherresponse.weather[0].main}");
                 Console.WriteLine($"Wind: {weatherresponse.wind.gust}");
